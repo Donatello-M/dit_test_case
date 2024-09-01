3d298f1edb17920a23e90d96d9c8f64ca91d5b4a
Danila Makarichev <72694196+Donatello-M@users.noreply.github.com>
2024-09-02 00:23:10 +0300

    Update admin.py

--- room_booking/admin.py
+++ room_booking/admin.py
@@ -5,6 +5,6 @@ from room_booking import models
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    """ Админка для Poll """
+    """ Админка для Room """
     list_display = ('name',)
     search_fields = ('name',)
